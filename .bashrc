abefc635a3e2c69af621281996fbbea9d9c85926
Bryan Whiting <bryan.whiting@bateswhite.com>
2017-05-23 18:47:02 -0400

    Created time entry processing script

--- .bashrc
+++ .bashrc
@@ -1,5 +1,8 @@
 
+# ONLY RUN ON WINDOWS
 export PATH=$PATH:/c/Program\ Files/Rstudio/bin/
+export PATH=$PATH:/c/Users/bwhiting/Documents/R/R-3.3.3/bin/
+
 # Make data science directory
 alias mkdsd="mkdir Scripts Temp Output Log"
 
@@ -13,9 +16,11 @@ cdls() { cd "$@" && ls; }
 td() { echo -e "☐ $@\n$(cat ~/my.todo)" > ~/my.todo;}
 tdn() { echo -e "☐ NAB: $@\n$(cat ~/my.todo)" > ~/my.todo;}
 
-tent() { echo -e "TIME: $(date +%Y%m%d-%H%M%S) | PROJ: $1 | DESC: ${@:2:999}\n$(cat ~/my.todo)" > ~/my.todo;}
+tent() { echo -e "TIME: $(date +%Y%m%d-%H%M%S) | PROJ: $1 | DESC: ${@:2:999}\n$(cat ~/timesheet.txt)" > ~/timesheet.txt;}
+gettime() { RScript.exe "/h/github/ds-references/rscripts/time-entry.R";}
 
 # Full recursive directory listing
 alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'' | less'
 
+# Open an Excel file from the command line
 excel() { "C:\Program Files (x86)\Microsoft Office\Office16\EXCEL.EXE" "$@";}
---
+++ rscripts/time-entry.R
@@ -0,0 +1,60 @@
+# Compile time entry
+
+options(warn = -1)
+suppressMessages(library(dplyr))
+suppressMessages(library(magrittr))
+suppressMessages(library(lubridate))
+df <- read.table("H:/timesheet.txt", sep = "|")
+colnames(df) <- c("time", "proj", "desc")
+df$time %<>% gsub("TIME: ", "", ., fixed = T) %>% trimws()
+df$proj %<>% gsub("PROJ: ", "", ., fixed = T) %>% trimws()
+df$desc %<>% gsub("DESC: ", "", ., fixed = T) %>% trimws()
+
+df %<>% mutate(dt = ymd_hms(time, tz = "America/New_York"),
+               date = date(dt)) %>% arrange(dt)
+
+# Calculate time as difference between current and the lead
+df %<>% group_by(date) %>%
+  mutate(lead_dt = lead(dt), 
+         entry = seq_along(time)) %>%
+  mutate(difftime = lead_dt - dt)
+
+# Replace the last entry with the current time (to show how long since last)
+now <- ymd_hms(Sys.time(), tz = "America/New_York")
+df %<>% group_by(date) %>% 
+  # Multiply by 60 because it calculates differences in minutes
+  mutate(difftime = ifelse(entry == n() & date == Sys.Date(), (now - dt)*60, difftime)) 
+
+# Compute total times
+df %<>%
+  mutate(tot_min = round(difftime / 60, 2)) %>%
+  mutate(tot_hr = round(tot_min / 60, 2))
+  
+
+# Remove lines with "eofd" or "done" 
+df %<>% filter(!(proj %in% c("eofd", "done")))
+
+# SUMMARY STATS --------------------------------------------
+# Total:
+# Today:
+cat("\nTime by project today:\n")
+df %>% filter(date == Sys.Date()) %>%
+  group_by(proj) %>%
+  summarize(`By min` = sum(tot_min),
+            `By hr` = sum(tot_hr),
+            desc = toString(desc)) %>%
+  rename(Project = proj) %>%
+  as.data.frame() %>%
+  print()
+  
+curr <- df %>% ungroup() %>%
+  filter(date == Sys.Date()) %>%
+  filter(entry == n()) %>%
+  select(tot_min) %>%
+  as.matrix()
+
+cat("\nTime on current task: ", curr, " min\n")
+
+
+
+options(warn = 0)
